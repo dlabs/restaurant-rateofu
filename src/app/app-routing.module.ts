b0275c17fc18ee94f26de33df1bb7a61eaf45fa8
Matej <matej.korbar>
2021-03-30 10:30:01 +0200

    Set up routing for submit order

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SubmitOrderComponent } from './guest/submit-order/submit-order.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'guest', component: SubmitOrderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
