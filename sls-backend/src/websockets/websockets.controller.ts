d786d9c05ae5491575217fe6af9f4b23ab8dabb0
Max K <max@ezic.io>
2021-10-09 16:10:38 +0300

    204 for hook

--- sls-backend/src/websockets/websockets.controller.ts
+++ sls-backend/src/websockets/websockets.controller.ts
@@ -1,14 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { WsConnectionService } from './services/ws-connection.service';
 
 @Controller('ws')
 export class WebsocketsController {
     constructor(private readonly wsConnectionService: WsConnectionService) {}
 
+    @HttpCode(204)
     @Post('register')
     public async registerWsConnection(@Body() data: { connectionId: string }) {
         // * This method is executed by $connect hook on AWS API Gateway
         await this.wsConnectionService.registerConnection(data.connectionId);
-        return JSON.stringify(data);
+        return;
     }
 }
