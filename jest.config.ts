b76a4dbe4b7cc297d80f6e6bda3c393b824124b2
Ivan Kolundžija <ivankolundzija@gmail.com>
2022-04-24 18:31:09 +0200

    Configure tests to not be compiled to dist

--- jest.config.ts
+++ jest.config.ts
@@ -3,4 +3,6 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     setupFiles: ['./jest.setup.ts'],
+    // Add /dist/ to the ignore pattern to avoid testing tsc output
+    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
