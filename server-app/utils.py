e5268f54439c347d9eb4a51ff0cadd6847026cdb
Ales Kancilija <ales.kancilija@hotmail.com>
2022-09-11 21:50:02 +0200

    Add docstrings to generate_* methods

--- server-app/utils.py
+++ server-app/utils.py
@@ -7,6 +7,12 @@ LOWER_CHARS_AND_DIGITS = ascii_lowercase + digits
 ORDER_STAGES = ('ordered', 'preparing', 'ready_to_serve', 'delivered')
 
 def generate_bearer_token(length=6):
+    """
+    Generates an ID, containing lower and upper chars and digits
+    Parameters:
+    - int length (default=6)
+    Example or result: 'kfByOn'
+    """
     assert length > 6, 'parameter "length" must be 6 or higher'
 
     # generate random hash, with custom length
@@ -14,6 +20,10 @@ def generate_bearer_token(length=6):
     return ''.join([ALL_CHARS_AND_DIGITS[randrange(0,len_all)] for _ in range(length)])
 
 def generate_id():
+    """
+    Generates an ID, containing lower chars and digits
+    Example of result: 'sla4w3o5-23na-ybqo-9y3o-g3hlkl7qo223'
+    """
     len_lower = len(LOWER_CHARS_AND_DIGITS)
     parts = [
         ''.join([LOWER_CHARS_AND_DIGITS[randrange(0,len_lower)] for _ in range(8)]),
