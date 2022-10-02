c3d4024ea8746c552d9e547656d477c014d636a3
Rron Jakupi <rronjk@gmail.com>
2022-10-02 22:48:10 +0200

    added seed

--- server/src/modules/menuitem/menuitem.service.ts
+++ server/src/modules/menuitem/menuitem.service.ts
@@ -1,15 +1,28 @@
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import { BaseService } from '@common/services';
-import { Inject, Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { RepositoryProviderTypes } from 'modules/database/repository.provider.types';
 import { MenuItemDocument } from './entities/menuitem.entity';
+import { menuItems } from 'modules/database/seeds/seed';
 
 @Injectable()
-export class MenuItemService extends BaseService<MenuItemDocument> {
+export class MenuItemService
+  extends BaseService<MenuItemDocument>
+  implements OnModuleInit
+{
   constructor(
     @Inject(RepositoryProviderTypes.MenuItemRepository)
     private readonly menuItemModel: Model<MenuItemDocument>,
   ) {
     super(menuItemModel);
+  }
+
+  // Seed data
+  async onModuleInit() {
+    // If there's data already inserted, don't insert duplicates
+    const seedData = (await this.menuItemModel.find().count()) === 0;
+    if (seedData) {
+      await this.menuItemModel.insertMany(menuItems);
+    }
   }
 }
