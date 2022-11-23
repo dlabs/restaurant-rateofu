8e9248a21683b46d7873cc83342a910ab13ae1a3
Toni <toni.dezman@gmail.com>
2022-11-23 12:09:49 +0100

    fixes frontend

--- server-app/db/seeds.rb
+++ server-app/db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Generating Food Menu Items..."
+3.times do
+  Item.create(title: Faker::Food.dish, description: Faker::Food.description, item_type: "food", price_cents: rand(700..2_500))
+end
+
+puts "Generating Drink Menu Items..."
+2.times do
+  Item.create(title: "#{Faker::Tea.variety} drink", description: Faker::Food.description, item_type: "drink", price_cents: rand(200..1_000))
+end
+
+puts "Done! 🎉"
